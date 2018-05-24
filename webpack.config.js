52e472757840cede2d642a2322dedc9ded27b679
sis0k0 <stanimira.vlaeva@gmail.com>
2018-05-24 19:17:13 +0300

    refactor: update to use the provided host from nativescript-dev-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
-const { normalize, relative, resolve, join, parse } = require("path");
-const { statSync } = require("fs");
+const { join, relative, resolve, sep } = require("path");
 
 const webpack = require("webpack");
 const nsWebpack = require("nativescript-dev-webpack");
 const nativescriptTarget = require("nativescript-dev-webpack/nativescript-target");
+const { PlatformReplacementHost } = require("nativescript-dev-webpack/host/platform");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const { NativeScriptWorkerPlugin } = require("nativescript-worker-loader/NativeScriptWorkerPlugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
-const { NodeJsSyncHost } = require("@angular-devkit/core/node");
-const { virtualFs } = require("@angular-devkit/core");
 const { AngularCompilerPlugin } = require("@ngtools/webpack");
 
 module.exports = env => {
@@ -26,13 +23,10 @@ module.exports = env => {
         throw new Error("You need to provide a target platform!");
     }
 
-    const host = new NodeJsSyncHost();
-    // const platformHost = new virtualFs.PatternMatchingHost(host);
-    const platformHost = new PlatformReplacementHost(host, ["tns", platform]);
- 
-    const platforms = ["ios", "android"];
+    const extensions = ["tns", platform];
+    const platformHost = new PlatformReplacementHost(extensions);
+
     const projectRoot = __dirname;
-    nsWebpack.loadAdditionalPlugins({ projectDir: projectRoot });
 
     // Default destination inside platforms/<platform>/...
     const dist = resolve(projectRoot, nsWebpack.getAppPath(platform, projectRoot));
@@ -45,28 +39,21 @@ module.exports = env => {
         appPath = "app",
         appResourcesPath = "app/App_Resources",
 
-        // Aot, snapshot, uglify and report can be enabled by providing
-        // the `--env.snapshot`, `--env.uglify` or `--env.report` flags
-        // when running 'tns run android|ios'
-        aot,
-        snapshot,
-        uglify,
-        report,
+        // You can provide the following flags when running 'tns run android|ios'
+        aot, // --env.aot
+        snapshot, // --env.snapshot
+        uglify, // --env.uglify
+        report, // --env.report
     } = env;
-    const ngToolsWebpackOptions = { tsConfigPath:
-        join(__dirname, 
-            aot ? "tsconfig.aot.json" : "tsconfig.tns.json"
-        )
-    };
 
     const appFullPath = resolve(projectRoot, appPath);
     const appResourcesFullPath = resolve(projectRoot, appResourcesPath);
 
     const entryModule = aot ?
-        "main.aot.ts" :
-        "main.ns.ts";
-    const entryPath = `./${entryModule}`;
-    const vendorPath = `./vendor.ts`;
+        nsWebpack.getAotEntryModule(appFullPath) : 
+        `${nsWebpack.getEntryModule(appFullPath)}.ts`;
+    console.log(entryModule)
+    const entryPath = `.${sep}${entryModule}`;
 
     const config = {
         mode: uglify ? "production" : "development",
@@ -81,7 +68,6 @@ module.exports = env => {
         target: nativescriptTarget,
         entry: {
             bundle: entryPath,
-            vendor: vendorPath,
         },
         output: {
             pathinfo: false,
@@ -105,7 +91,6 @@ module.exports = env => {
             symlinks: true
         },
         resolveLoader: {
-            // don't resolve symlinks to symlinked loaders
             symlinks: false
         },
         node: {
@@ -118,7 +103,6 @@ module.exports = env => {
         },
         devtool: "none",
         optimization: {
-            runtimeChunk: { name: "vendor" },
             splitChunks: {
                 cacheGroups: {
                     vendor: {
@@ -135,7 +119,6 @@ module.exports = env => {
             },
             minimize: !!uglify,
             minimizer: [
-                // Override default minimizer to work around an Android issue by setting compress = false
                 new UglifyJsPlugin({
                     uglifyOptions: {
                         parallel: true,
@@ -199,7 +182,7 @@ module.exports = env => {
                 {
                     test: /.ts$/, use: [
                         "nativescript-dev-webpack/moduleid-compat-loader",
-                        { loader: "@ngtools/webpack", options: ngToolsWebpackOptions },
+                        "@ngtools/webpack",
                     ]
                 },
 
@@ -231,46 +214,26 @@ module.exports = env => {
                 { from: "fonts/**" },
                 { from: "**/*.jpg" },
                 { from: "**/*.png" },
-                { from: "**/*.xml" },
             ], { ignore: [`${relative(appPath, appResourcesFullPath)}/**`] }),
             // Generate a bundle starter script and activate it in package.json
             new nsWebpack.GenerateBundleStarterPlugin([
                 "./vendor",
                 "./bundle",
             ]),
-            // Support for web workers since v3.2
+            // For instructions on how to set up workers with webpack
+            // check out https://github.com/nativescript/worker-loader
             new NativeScriptWorkerPlugin(),
 
-            new AngularCompilerPlugin(
-                Object.assign({
-                    entryModule: resolve(__dirname, "app/app.module.tns#AppModule"),
-                    skipCodeGeneration: !aot,
-                    host: platformHost,
-                }, ngToolsWebpackOptions)
-            ),
-
+            new AngularCompilerPlugin({
+                host: platformHost,
+                entryModule: resolve(appPath, "app.module#AppModule"),
+                tsConfigPath: join(__dirname, aot ? "tsconfig.aot.json" : "tsconfig.tns.json"),
+                skipCodeGeneration: !aot,
+            }),
             // Does IPC communication with the {N} CLI to notify events when running in watch mode.
             new nsWebpack.WatchStateLoggerPlugin(),
         ],
     };
-
-    if (platform === "android") {
-        // Add your custom Activities, Services and other android app components here.
-        const appComponents = [
-            "tns-core-modules/ui/frame",
-            "tns-core-modules/ui/frame/activity",
-        ];
-
-        // Require all Android app components
-        // in the entry module (bundle.ts) and the vendor module (vendor.ts).
-        config.module.rules.unshift({
-            test: new RegExp(`${entryPath}|${vendorPath}`),
-            use: {
-                loader: "nativescript-dev-webpack/android-app-components-loader",
-                options: { modules: appComponents }
-            }
-        });
-    }
 
     if (report) {
         // Generate report files for bundles content
@@ -302,67 +265,3 @@ module.exports = env => {
 
     return config;
 };
-
-class PlatformReplacementHost {
-    constructor(_delegate, _platforms) {
-        this._delegate = _delegate;
-        this._platforms = _platforms;
-        this._patterns = [];
-    }
-
-    _resolve(path) {
-        const { dir, name, ext } = parse(path);
-
-        for (const platform of this._platforms) {
-            const newPath = join(dir, `${name}.${platform}${ext}`);
-
-            try {
-                const stat = statSync(newPath);
-                return stat && stat.isFile() ?
-                    newPath :
-                    path;
-            } catch(_e) {
-                // continue checking the other platforms
-            }
-        }
-
-        return path;
-    }
-
-    get capabilities() { return this._delegate.capabilities; }
-    write(path, content) {
-        return this._delegate.write(this._resolve(path), content);
-    }
-
-    read(path) {
-        return this._delegate.read(this._resolve(path));
-    }
-
-    delete(path) {
-        return this._delegate.delete(this._resolve(path));
-    }
-    rename(from, to) {
-        return this._delegate.rename(this._resolve(from), this._resolve(to));
-    }
-    list(path) {
-        return this._delegate.list(this._resolve(path));
-    }
-    exists(path) {
-        return this._delegate.exists(this._resolve(path));
-    }
-    isDirectory(path) {
-        return this._delegate.isDirectory(this._resolve(path));
-    }
-    isFile(path) {
-        return this._delegate.isFile(this._resolve(path));
-    }
-    // Some hosts may not support stat.
-    stat(path) {
-        return this._delegate.stat(this._resolve(path));
-    }
-    // Some hosts may not support watching.
-    watch(path, options) {
-        return this._delegate.watch(this._resolve(path), options);
-    }
-}
-
